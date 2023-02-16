308a5f5897928424095820554663a05ddfa7fd68
Andrew Olivares <Wrestler533438@gmail.com>
2023-02-16 14:16:39 -0800

    Adding styles to Rest-rant

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+//Modules
 const express = require('express')
 const app = express()
 require('dotenv').config()
 
+//Express
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+
+// Routes and controllers
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -15,4 +21,5 @@ app.get('*', (req, res) => {
     res.status(404).render('error404')
 })
 
+// Listen
 app.listen(process.env.PORT)
