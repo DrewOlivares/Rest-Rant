eade80ae77f6dde4b9125da3944bb447538fd906
Andrew Olivares <Wrestler533438@gmail.com>
2023-02-16 16:34:24 -0800

    jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,24 @@
 const router = require('express').Router()
 
-
+let places = [{
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: '/images/restaurant1.jpg'
+}, {
+  name: 'Coding Cat Cafe',
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Coffee, Bakery',
+  pic: '/images/restaurant2.jpg'
+}]
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/restaurant1.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/restaurant2.jpg'
-      }]
-
     res.render('./places/index', {places})
 })
 
